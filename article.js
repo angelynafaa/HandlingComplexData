a7c21c9506b59978c49a81508a87adc28c4999a8
angelyna <anelyna826@gmail.com>
2020-03-23 08:53:22 -0500

    first commit

---
+++ article.js
@@ -0,0 +1,8 @@
+const article ={
+    id:1,
+    title: "Lorem Ipsum Dollor",
+    featuredImage: "https://i.piscum.phoyos/id/204/536/354.jpg?grayscale",
+    description:"lorem ipsum is simpky dummy text of the printing and typesting."
+}
+
+export default article;
